f5828e732f6884769a4286326dabc5d76f30b18e
Vijendra Saini <vijendra.saini@dotsquares.com>
2025-01-30 19:36:02 +0530

    --

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { CategoryDetailComponent } from './web/component/category/category-detai
 import { HomeIndexComponent } from './web/component/_home/home-index/home-index.component';
 import { NotFoundComponent } from './web/component/_shared/not-found/not-found.component';
 import { UserLoginComponent } from './web/component/user/user-login/user-login.component';
+import { UserRegistrationComponent } from './web/component/user/user-registration/user-registration.component';
+import { ForgotPasswordComponent } from './web/component/user/forgot-password/forgot-password.component';
 // import { LoginComponent } from './web/component/user/login/login.component';
 // import { RegisterComponent } from './web/component/user/register/register.component';
 // import { MyProfileComponent } from './web/component/user/my-profile/my-profile.component';
@@ -16,7 +18,8 @@ export const routes: Routes = [
     { path: 'admin/categories/add', component: AddCategoryComponent },
     { path: '', component: HomeIndexComponent }, // Default route
     { path: 'login', component: UserLoginComponent },
-    // { path: 'register', component: RegisterComponent },
+    { path: 'register', component: UserRegistrationComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent },
     // { path: 'myprofile', component: MyProfileComponent },
     { path: 'product-category/:urlHandle', component: CategoryDetailComponent },
     { path: '**', component: NotFoundComponent } // Wildcard for unknown paths
